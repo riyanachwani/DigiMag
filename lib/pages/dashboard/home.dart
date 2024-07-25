dc60946383ac45fba539b72c008c28ff30c7ea6d
riyanachwani <riyanachwani22@gmail.com>
2024-07-25 20:21:52 +0530

    articles fetched on homepage

--- lib/pages/dashboard/home.dart
+++ lib/pages/dashboard/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:digimag/utils/api_services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +9,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late Future<List<Article>> _articlesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _articlesFuture = ApiService().getLatestNews();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +26,33 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(fontFamily: "RosebayRegular", fontSize: 20),
         ),
         automaticallyImplyLeading: false,
+      ),
+      body: FutureBuilder<List<Article>>(
+        future: _articlesFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No articles available.'));
+          } else {
+            final articles = snapshot.data!;
+            return ListView.builder(
+              itemCount: articles.length,
+              itemBuilder: (context, index) {
+                final article = articles[index];
+                return ListTile(
+                  title: Text(article.title),
+                  subtitle: Text(article.description),
+                  onTap: () {
+                    // Handle article tap
+                  },
+                );
+              },
+            );
+          }
+        },
       ),
     );
   }
