e859016be3560389130564dcd9e4f0371380220f
riyanachwani <riyanachwani22@gmail.com>
2024-08-06 18:26:02 +0530

    Search Page Updated

--- lib/pages/dashboard/search.dart
+++ lib/pages/dashboard/search.dart
@@ -1,4 +1,7 @@
+import 'package:digimag/utils/api_services.dart';
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -8,6 +11,26 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  String query = '';
+  List<Article> articles = [];
+  bool isLoading = false;
+
+  void searchArticles(String query) async {
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      articles = await ApiService().searchArticles(query);
+    } catch (e) {
+      print('Error searching articles: $e');
+    }
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,15 +43,58 @@ class _SearchPageState extends State<SearchPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: TextField(
-          decoration: InputDecoration(
-            prefixIcon: Icon(Icons.search),
-            hintText: 'Search Articles',
-            border: OutlineInputBorder(),
-          ),
-          onChanged: (value) {
-            setState(() {});
-          },
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                hintText: 'Search Articles',
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (value) {
+                query = value;
+                if (query.isNotEmpty) {
+                  searchArticles(query);
+                } else {
+                  setState(() {
+                    articles = [];
+                  });
+                }
+              },
+            ),
+            SizedBox(height: 10),
+            isLoading
+                ? CircularProgressIndicator()
+                : Expanded(
+                    child: ListView.builder(
+                      itemCount: articles.length,
+                      itemBuilder: (context, index) {
+                        final article = articles[index];
+                        return ListTile(
+                          title: Text(
+                            article.title,
+                            style: TextStyle(
+                              fontFamily: "RosebayRegular",
+                              color: Colors.black,
+                              fontSize: 16.0,
+                            ),
+                          ),
+                          subtitle: Text(
+                            article.description,
+                            style: TextStyle(
+                              // fontFamily: "RosebayRegular",
+                              color: Colors.black,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                          onTap: () {
+                            // Handle article tap, e.g., navigate to detail page
+                          },
+                        );
+                      },
+                    ),
+                  ),
+          ],
         ),
       ),
     );
